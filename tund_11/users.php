97f0cd55409596a51bb74e34eebb0bfcfd1c1e1a
Emma Loore Tae <emmatae@users.noreply.github.com>
2018-11-20 11:34:27 +0200

    tund_11

---
+++ tund_11/users.php
@@ -0,0 +1,52 @@
+<?php
+  require("functions.php");
+  //kui pole sisse loginud
+  if(!isset($_SESSION["userId"])){
+	  header("Location: index_1.php");
+	  exit();
+  }
+  
+  //väljalogimine
+  if(isset($_GET["logout"])){
+	session_destroy();
+	header("Location: index_1.php");
+	exit();
+  }
+  
+  $userslist = listusers();
+  
+  $pageTitle = "Kõik kasutajad:";
+  require("header.php");
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>
+	  <?php
+	    echo $_SESSION["firstName"];
+		echo " ";
+		echo $_SESSION["lastName"];
+	  ?>
+	, õppetöö</title>
+	<style>
+	  <?php
+        echo "body{background-color: " .$_SESSION["bgColor"] ."; \n";
+		echo "color: " .$_SESSION["txtColor"] ."} \n";
+	  ?>
+	</style>
+  </head>
+  <body>
+ 
+	<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+	<hr>
+	<?php echo $userslist; ?>
+	<hr>
+	<ul>
+	  <li><a href="?logout=1">Logi välja</a>!</li>
+	  <li><a href="main.php">Tagasi pealehele</a></li>
+	</ul>
+	
+  </body>
+</html>
